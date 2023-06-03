b7e89fbe8db7d0df90df142a02c75f0c4e37b561
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-03 14:20:33 -0400

    Initial Commit

---
+++ DogPictures/DogPicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicturesApp.swift
+//  DogPictures
+//
+//  Created by Lori Rothermel on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DogPicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
